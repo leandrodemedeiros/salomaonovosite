e030f353ede9bedddaa10bcf35d2dd7c89cce418
leandrodemedeiros <77240253+leandrodemedeiros@users.noreply.github.com>
2024-01-07 17:09:05 -0300

    Update AboutHero.jsx

--- components/AboutHero.jsx
+++ components/AboutHero.jsx
@@ -18,12 +18,9 @@ const AboutHero = () => {
         <div className="row">
           <div className="col-12 col-md-8 col-lg-8 mx-auto">
             <div className="about-content text-center">
-              <h1>Reimagining real estate to make it easier to unlock.</h1>
+              <h1>Somos uma agência de imóvies que preza pelo seu bem-estar.</h1>
               <p>
-                On the other hand, we denounce with righteous indignation and
-                dislike men who are so beguiled and demoralized by the charms of
-                pleasure of the moment, so blinded by desire, that they cannot
-                foresee the pain and trouble.
+                Conheça a nossa história. Nós queremos ajudar você a continuar construindo a sua.
               </p>
             </div>
           </div>
